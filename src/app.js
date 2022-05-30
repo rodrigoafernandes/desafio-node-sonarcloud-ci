8efb857b75a95416dbd4494fa7d37bfc8bf75776
Rodrigo Fernandes <rodrigoforit@gmail.com>
2022-05-30 16:15:12 -0300

    Fixes sonar issues

--- src/app.js
+++ src/app.js
@@ -1,19 +1,20 @@
 const express = require("express");
 const router = express.Router();
 
-const appDesafio = express();
+const app = express();
 
-appDesafio.use(express.json());
-appDesafio.use(
+app.use(express.json());
+app.use(
   express.urlencoded({
     extended: true,
   })
 );
+app.disable("x-powered-by");
 
 router.get("/", async function (req, res, next){
   res.json({ 'message': 'Full Cycle Rocks!' })
 });
 
-appDesafio.use("/", router);
+app.use("/", router);
 
-module.exports = appDesafio;
+module.exports = app;
